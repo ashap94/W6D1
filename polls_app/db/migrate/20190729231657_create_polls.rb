37c54a815455004ec25822a9d7253b565a27306c
ashap94 <aaronshapiro94@gmail.com>
2019-07-29 17:52:34 -0700

    Add files from W6D1 to git folder

---
+++ polls_app/db/migrate/20190729231657_create_polls.rb
@@ -0,0 +1,9 @@
+class CreatePolls < ActiveRecord::Migration[5.2]
+  def change
+    create_table :polls do |t|
+      t.integer :author_id
+      t.string :title
+    end
+    add_index :polls, :author_id
+  end
+end
